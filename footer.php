b944947c81ee48fe4f6ebc9ba9a792043fd34561
Konrad Förstner <konrad@foerstner.org>
2010-05-30 13:51:01 +0200

    Add Creative Commons link

--- footer.php
+++ footer.php
@@ -3,6 +3,9 @@
 		<div id="tags"><?php if (Plugins::is_loaded('tagcloud')) $theme->tag_cloud(); else $theme->show_tags();?></div>
 	</div>
 	<div id="footer">
+	        <p>
+<a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/de/88x31.png" /></a> <br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/">Creative Commons Attribution 3.0 Germany License</a> by <a href="http://konrad.foerstner.org/">Konrad Förstner</a>.
+                </p>
 		<p>
 			<?php printf( _t('%1$s is powered by %2$s'), Options::get('title'),' <a
 			href="http://www.habariproject.org/" title="Habari">Habari ' . Version::HABARI_VERSION  . '</a>' ); ?> - 
